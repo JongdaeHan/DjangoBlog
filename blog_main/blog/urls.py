7e220caed6534a70115c10e57825f4214a8d505b
HYERINI <rinlee2001@naver.com>
2022-04-08 17:22:42 +0900

    카테고리 뷰 생성, 태그 모델 추가

--- blog_main/blog/urls.py
+++ blog_main/blog/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     # path('',views.index),
     # path('<int:pk>/',views.single_post_page),
     path('<int:pk>/',views.PostDetail.as_view()),
+    path('category/<str:slug>/',views.show_category_posts)
 ]
